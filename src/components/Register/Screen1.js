c0924926b8664de69b4804b983f65bbf495fde75
Harsh Bhatia <zest2019.trainee062@gmail.com>
2019-02-23 15:56:10 +0530

    Register UI - Screen 2 UI

--- src/components/Register/Screen1.js
+++ src/components/Register/Screen1.js
@@ -1,73 +1,80 @@
 import React, {Component} from 'react'
-import {StyleSheet,View, Text,TextInput, Button,Image,TouchableHighlight} from 'react-native'
+import {View, Text, TouchableOpacity,TextInput, StyleSheet, Image,} from 'react-native'
+
 import { Dimensions } from "react-native";
-// import ModalPicker from 'react-native-modal-picker';
 
 var width = Dimensions.get('window').width; //full width
 var height = Dimensions.get('window').height; //full height
 
-class Register extends Component {
 
-    reg() {
-        firstname = "Harsh"
-        lastname = "Bhatia"
-        email = "yjyjessoss-3179@yopmail.com"
-        password = "Test@123"
-        confirmPassword = "Test@123"
-        jobTitle = "intern"
-        companyName = "cygnet"
-        countryId = "91"
-        phoneNumber = "8905003200"
-        userLatitude = "4.092356",
-        userLongitude = "-56.062161",
-        userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0 Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0",
-        userTimeZoneOffSet = "+05:30"
+class Screen1 extends Component {
 
-        console.warn(firstname,lastname,email,password,confirmPassword,jobTitle,countryId,
-            phoneNumber,userLatitude,userLongitude,userAgent,userTimeZoneOffSet)
+    state = {
+        register: null,
+        message: ''
+    }
 
+    checkuser(text) {
+        ema= text
 
-            return fetch('http://cygnatureapipoc.stagingapplications.com/api//account/register', {
-            method: 'POST',
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+        
+        if(reg.test(ema) === false)
+        {
+        return false;
+        }
+        else {
+            this.setState({email:ema})
+         
+        return fetch('http://cygnatureapipoc.stagingapplications.com/api/account/check-user-exists/'+(ema), {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                firstname: firstname,
-                lastname: lastname,
-                email: email,
-                password: password,
-                confirmPassword: confirmPassword,
-                jobTitle: jobTitle,
-                companyName: companyName,
-                countryId: countryId,
-                phoneNumber: phoneNumber,
-                userLatitude: userLatitude,
-                userLongitude: userLongitude,
-                userAgent: userAgent,
-                userTimeZoneOffSet: userTimeZoneOffSet
-            }),
         }).then((response) => response.json())
         .then((responseJson) => {
-            console.warn(responseJson["message"])
+           
+            bva=responseJson["data"]
+            message=responseJson["message"]
+            this.setState({register: bva[0], message: message})
+            //console.warn(this.state.register)
             
         })
         .catch((error) => {
             console.warn(error);
         });
+        }
+    }
 
-
+    next() {
+        const { register } = this.state
+        console.warn("REDIRECT - UI 2")
+        if (register == null) {
+            console.warn("Check user failed")
+        } else {
+            console.warn(register)
+        }
     }
 
      render() {
          return (
+             
+
+
+<View style={{flex: 1}}>
+            <View style={{width: width, height: height - 90}}>
+            
             <View style={styles.container}>
+            
+            <Image
+                source={require('../../../img/logo.png')}
+                />
             <Text style = { styles.boxTitle }>Personal Details</Text>
-              <View style={styles.inputContainer}>
+
+            <View style={styles.inputContainer}>
                 <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/male-user/ultraviolet/50/3498db'}}/>
                 <TextInput style= { styles.inputs }
                      returnKeyType="next"
-                     keyboardType="email-address"
                      placeholder="First Name"
                      placeholderTextColor="gray"
                      underlineColorAndroid='transparent'
@@ -76,12 +83,10 @@ class Register extends Component {
 
                 />
                 </View>
-
                 <View style={styles.inputContainer}>
                 <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/male-user/ultraviolet/50/3498db'}}/>
                 <TextInput style= { styles.inputs }
                      returnKeyType="next"
-                     keyboardType="email-address"
                      placeholder="Last Name"
                      placeholderTextColor="gray"
                      underlineColorAndroid='transparent'
@@ -100,8 +105,12 @@ class Register extends Component {
                      placeholderTextColor="gray"
                      underlineColorAndroid='transparent'
                      autoCapitalize="none"
+                     onChangeText={text => this.checkuser(text)}
                      autoCorrect={false}
                 />
+                </View>
+                <View>
+                {this.state.register ? <Text style={styles.boxTitle} title="ERROR"/> : null}
                 </View>
 
                 <View style={styles.inputContainer}>
@@ -120,7 +129,7 @@ class Register extends Component {
                 <View style={styles.inputContainer}>
                 <Image style={styles.inputIcon} source={{uri: 'https://img.icons8.com/ultraviolet/2x/password.png'}}/>
                 <TextInput style= { styles.inputs }
-                     returnKeyType="next"
+                     returnKeyType="done"
                      placeholder="Confirm Password"
                      secureTextEntry={true}
                      placeholderTextColor="gray"
@@ -129,44 +138,57 @@ class Register extends Component {
                      autoCorrect={false}
                 />
                 </View>
-                <View style={styles.buttonContainer}>
-                    <Button style={styles.signUpText} title="Next" onPress={_ =>this.reg()}/>
+            </View>
+            </View>{/* Main container end */}
+
+
+            <View style={{width: width, height: 200}}>
+            
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={_ => this.next()} style={{backgroundColor:'blue', margin: 10}}>
+    <Text style={styles.signUpText}>Next</Text>
+</TouchableOpacity>
                 </View>
-            </View> 
-                   
-            )
+            
+            </View>
+        </View>
+                )
          }
      }
 
-export default Register
+export default Screen1
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#7f8fa6',
+        marginTop: -110,
+        backgroundColor: '#A3CB38',
         width: width,
         height: height,
         resizeMode: 'cover',
         justifyContent:'center',
         alignItems:'center',
+        flex: 1
     },
     inputContainer: {
         backgroundColor: 'rgba(255,255,255,0.7)',
-        borderRadius:30,
-        width:250,
+        borderRadius:40,
+        width:width,
         height:45,
         marginBottom:10,
         marginTop:10,
-        marginLeft:20,
-        marginRight: 20,
         flexDirection: 'row',
         alignItems:'center',
     },
+    error: {
+        color: 'red'
+    },
     inputs: {
-        height:45,
+    
+        height:height,
         marginLeft:16,
         borderBottomColor: '#FFFFFF',
         flex:1,
-        fontSize: 15,
+        fontSize: 20,
     },
     inputIcon:{
         width:25,
@@ -175,23 +197,26 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
       },
     boxTitle:{
-        margin: 5,
-        fontSize: 22,
-        color: '#003d5a'
+        margin: 10,
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white'
     },
     buttonContainer: {
-        height:60,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop:170, 
-        width:1000,
-        borderRadius:30,
-        backgroundColor: "#718093",
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        width:width,
+        backgroundColor: "#833471",
+        
       },
       signUpText: {
         color: '#ffffff',
-        textAlign: 'right',
+        textAlign: 'center',
+        fontSize:25,
+        width:110,
+        height:50,
+        textAlignVertical: 'center',
         
       }
 })
