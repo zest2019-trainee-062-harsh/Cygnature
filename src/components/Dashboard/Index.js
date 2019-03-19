d9c1acb9be629cd686d66a63a0ecd12f8e7ceff3
Krishal Shah <zest2019.trainee009@gmail.com>
2019-03-19 16:44:12 +0530

    Integrated - Harsh(Document Upload)

--- src/components/Dashboard/Index.js
+++ src/components/Dashboard/Index.js
@@ -7,6 +7,9 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import { Dimensions } from "react-native";
 import moment from 'moment';
 
+import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
+
+
 import  Documents from '../Documents/Index.js'
 import  Contacts from '../Contacts/Index.js'
 import  Settings from '../Settings/General.js'
@@ -94,8 +97,54 @@ class Dashboard extends Component {
     }
 
     floatClicked= async() => {
-        alert(AsyncStorage.getItem('otp_check'))
-        alert("clicked")
+        //alert(AsyncStorage.getItem('otp_check'))
+        //alert("clicked")
+
+        DocumentPicker.show({
+            filetype: [DocumentPickerUtil.allFiles()],
+          },(error,res) => {
+
+             
+       var file = {
+       }
+
+       const formData = new FormData()
+       formData.append('file',{
+           
+        uri: res.uri,
+        type: res.type,
+        name: res.fileName,
+       })
+       //console.warn(res.uri+res.type+res.fileName)
+       //console.warn(formData)
+       
+            
+ return fetch('http://cygnatureapipoc.stagingapplications.com/api/document/upload',{
+    method: 'POST',
+    headers: {
+        'Authorization':this.state.auth,
+        //'Content-Type':'multipart/form-data'
+        // 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.N9w0jV/v+9PKjqTQVRpre2jTzBser7/a/Thzwn6/3UCGTehr+Gk9tZrbfUo0jndFhKFx2ywj3gA0kjDn9uf/mrfnjcwIru3WHKGL1lhTCxH/Uf7NYBtBbAd5M1f77b2aqFJHSXiQ0miSqKUB2cipfOf88VU+XiCO57FQtQzRFX0BtR5LZHaZQqSMMj8y47n6M86I6CKl/SntvdAyzVMh7uXFWsaYHY/nKmyIunJgewZM3q2ImLEx7ndLwRT1Kpus2er5pnvq19gX43RfAEkl80kK7axwGX2rPYYpoedDBXS35npshScOXwmZhiv36CmefEFHYLgFb83BWVafahApZ5huYSa1DHVNA6rCFJmrEdIZSVy/3U3prOcyiOsRM11DwsXwuJoOVYlwJgvluzUgDz32moOaTde6a1vkrdMaedYDyNNolAGSQ1Pu3+CKxRmp2tRpNY7GaajQVLaie574mFc2BGCwJdrueGAA8DuCPCgN2fpVlMYrufbYI7om3MnSjypSyoFuWg4O+4PG72+Qm5HvUfADtSbX4REh6XWBwyt89NRYf9f/qp/S3aLWZ8XsY5akfYKBECZQ6H6Z3rVRxAW7OgnqlPlAMBSw+DqAi3+28ActC0gqb3KOiDJFb3jIT4OoAMBRAA3hdAmblTr6EwpPmbxXqoCZ2CFL/PQqA/OTuKiBadJ1ZxkxCuFcb2Cl2J1fHnRKKWqv3CY4UMyBVkIFH2zGCh3g5IgaG3hH6IYaM2xrtMNJ2AZRByaG0ki5r99ydraBgmbN6OhEaKYlnG5RqR3tuIpNieQDbe6hFZEpvqk8iOk4pD2/nm2gBbymmt9bQcX1giYgYVUgsOUBytjTawP4g1BeJ0Rt0w0ev/jotRYNpxQaWs5aMGMYfdPW.ooqT_toFub_53Hn22ZdRhSAkcNnJrnwlDag93pWBvlA',
+    },
+    body: formData
+    }
+    ).then((response) => response.json())
+    .then((responseJson) => {
+        
+          
+            console.warn(responseJson["data"])
+        
+    
+    })
+    .catch((error) => {
+        console.warn(error.message);
+    });      
+          });
+
+
+          
+
+
     }
 
     render() {
