1f6400f69f5230ff78f76d4dc8ae8daa167524a6
Harsh Bhatia <zest2019.trainee062@gmail.com>
2019-03-25 17:46:44 +0530

    25 mar

--- src/components/Dashboard/Index.js
+++ src/components/Dashboard/Index.js
@@ -9,13 +9,13 @@ import { Dimensions } from "react-native";
 import moment from 'moment';
 import ActionButton from 'react-native-action-button';
 
-import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
+import { ProgressDialog } from 'react-native-simple-dialogs';
 
+import UploadModal from './UploadModal.js'
 
 import  Documents from '../Documents/Index.js'
 import  Contacts from '../Contacts/Index.js'
 import  Settings from '../Settings/General.js'
-import { bold } from 'ansi-colors';
 
 var width = Dimensions.get('window').width; //full width
 var height = Dimensions.get('window').height; //full width
@@ -41,6 +41,7 @@ class Dashboard extends Component {
         auth: null,
         documents: [],
         loading: true,
+        pdVisible: false,
         
     }
 
@@ -101,43 +102,7 @@ class Dashboard extends Component {
     }
 
     uploadDocument= async() => {
-
-        DocumentPicker.show({
-            filetype: [DocumentPickerUtil.allFiles()],
-            },(error,res) => {
-                
-       const formData = new FormData()
-       formData.append('file',{
-           
-        uri: res.uri,
-        type: res.type,
-        name: res.fileName,
-       })
-       //console.warn(res.uri+res.type+res.fileName)
-       //console.warn(formData)
-       
-            
-    return fetch('http://cygnatureapipoc.stagingapplications.com/api/document/upload',{
-    method: 'POST',
-    headers: {
-        'Authorization':this.state.auth,
-        //'Content-Type':'multipart/form-data'
-        // 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.N9w0jV/v+9PKjqTQVRpre2jTzBser7/a/Thzwn6/3UCGTehr+Gk9tZrbfUo0jndFhKFx2ywj3gA0kjDn9uf/mrfnjcwIru3WHKGL1lhTCxH/Uf7NYBtBbAd5M1f77b2aqFJHSXiQ0miSqKUB2cipfOf88VU+XiCO57FQtQzRFX0BtR5LZHaZQqSMMj8y47n6M86I6CKl/SntvdAyzVMh7uXFWsaYHY/nKmyIunJgewZM3q2ImLEx7ndLwRT1Kpus2er5pnvq19gX43RfAEkl80kK7axwGX2rPYYpoedDBXS35npshScOXwmZhiv36CmefEFHYLgFb83BWVafahApZ5huYSa1DHVNA6rCFJmrEdIZSVy/3U3prOcyiOsRM11DwsXwuJoOVYlwJgvluzUgDz32moOaTde6a1vkrdMaedYDyNNolAGSQ1Pu3+CKxRmp2tRpNY7GaajQVLaie574mFc2BGCwJdrueGAA8DuCPCgN2fpVlMYrufbYI7om3MnSjypSyoFuWg4O+4PG72+Qm5HvUfADtSbX4REh6XWBwyt89NRYf9f/qp/S3aLWZ8XsY5akfYKBECZQ6H6Z3rVRxAW7OgnqlPlAMBSw+DqAi3+28ActC0gqb3KOiDJFb3jIT4OoAMBRAA3hdAmblTr6EwpPmbxXqoCZ2CFL/PQqA/OTuKiBadJ1ZxkxCuFcb2Cl2J1fHnRKKWqv3CY4UMyBVkIFH2zGCh3g5IgaG3hH6IYaM2xrtMNJ2AZRByaG0ki5r99ydraBgmbN6OhEaKYlnG5RqR3tuIpNieQDbe6hFZEpvqk8iOk4pD2/nm2gBbymmt9bQcX1giYgYVUgsOUBytjTawP4g1BeJ0Rt0w0ev/jotRYNpxQaWs5aMGMYfdPW.ooqT_toFub_53Hn22ZdRhSAkcNnJrnwlDag93pWBvlA',
-    },
-    body: formData
-    }
-    ).then((response) => response.json())
-    .then((responseJson) => {
-        
-          
-            console.warn(responseJson["data"])
-        
-    
-    })
-    .catch((error) => {
-        console.warn(error.message);
-    });      
-          });
+        this.refs.UploadModal.show()
     }
 
     render() {
@@ -212,11 +177,11 @@ class Dashboard extends Component {
                         }
                         </ScrollView>
                     </View>
-                    <ActionButton buttonColor="#003d5a" bgColor="transparent">
-                        <ActionButton.Item buttonColor='#9b59b6' title="Upload File" onPress={() => this.uploadDocument()}>
+                    <ActionButton buttonColor="#003d5a" bgColor="rgba(255,255,255,0.8)">
+                        <ActionButton.Item buttonColor='#6eab52' title="Upload File" onPress={() => this.uploadDocument()}>
                             <Icon name="md-document" style={styles.actionButtonIcon} />
                         </ActionButton.Item>
-                        <ActionButton.Item buttonColor='#3498db' title="Add Signature" onPress={() => {}}>
+                        <ActionButton.Item buttonColor='#6eab52' title="Add Signature" onPress={() => {}}>
                             <Icon1 name="signature" style={styles.actionButtonIcon} />
                         </ActionButton.Item>
                     </ActionButton>
@@ -224,6 +189,7 @@ class Dashboard extends Component {
                         <Text style={styles.floatButtonText}>+</Text>
                     </TouchableOpacity> */}
                 </View>
+                <UploadModal ref={'UploadModal'}/>
             </View>
             )
         }
