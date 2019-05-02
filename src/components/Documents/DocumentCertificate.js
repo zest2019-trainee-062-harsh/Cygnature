a9169a2d075d1e4b2035ea491d55799f77220fc8
Mansi <zest2019-trainee-029MansiValecha>
2019-05-02 17:23:04 +0530

    finalcomit

--- src/components/Documents/DocumentCertificate.js
+++ src/components/Documents/DocumentCertificate.js
@@ -207,7 +207,6 @@ const styles = StyleSheet.create({
     mainContainer:{
         height: height*0.87,
         margin: 7,
-        marginTop:-7,
         borderWidth: 2,
         borderRadius:5,
         borderColor: "#003d5a",
